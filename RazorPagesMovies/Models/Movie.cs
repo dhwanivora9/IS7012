53fad8df685e70f3614bc86b2fd3780df940ee29
dhwanivora9 <54248325+dhwanivora9@users.noreply.github.com>
2019-09-07 18:59:07 -0400

    second change

--- RazorPagesMovies/Models/Movie.cs
+++ RazorPagesMovies/Models/Movie.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesMovies.Models
 {
-    public class Class
+    public class Movie
     {
+        public int ID { get; set; }
+        public int Title { get; set; }
+
+        [DataType(DataType.Date)]         
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal price { get; set; }
+
+
     }
 }
